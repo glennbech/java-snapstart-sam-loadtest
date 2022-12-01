05dfa6ec17ca0447a936fe60ce23b661f431037b
glennbech <glenn.bech@gmail.com>
2022-11-30 17:10:17 -0800

    Work function

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -3,7 +3,9 @@ package helloworld;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,6 +14,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import static java.math.BigInteger.ZERO;
 
 /**
  * Handler for requests to Lambda function.
@@ -22,14 +26,25 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-        }
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-            String output = "{ }";
-            return response.withStatusCode(200).withBody(output);
+        String output = String.valueOf(doSomeWorkPlease());
+        return response.withStatusCode(200).withBody(output);
+    }
+
+    /**
+     * Creates a randomly generated 100,000,0000 sized two-dimensional array with values between 0 and 42, and then sums them
+     *
+     * @return the sum of the randomly generated Array
+     */
+    public static BigInteger doSomeWorkPlease() {
+        int[][] matrix = new int[10000][10000];
+        for (int n = 0; n < matrix[0].length; n++) {
+            for (int j = 0; j < matrix[n].length; j++) {
+                matrix[n][j] = (int) (Math.random() * 42);
+            }
+        }
+        return Arrays.stream(matrix).flatMapToInt(Arrays::stream).mapToObj(BigInteger::valueOf).reduce(ZERO, BigInteger::add);
     }
 }
--- HelloWorldFunction/src/main/java/helloworld/MatrixAddition.java
+++ HelloWorldFunction/src/main/java/helloworld/MatrixAddition.java
@@ -7,14 +7,5 @@ import static java.math.BigInteger.*;
 
 public class MatrixAddition {
 
-    public static void main(String[] args) {
-        int[][] matrix = new int[10000][10000];
-        for (int n=0; n < matrix[0].length; n++) {
-            for (int j=0; j< matrix[n].length; j++ ) {
-                matrix[n][j] = 1;
-            }
-        }
-        System.out.println();
-        System.out.println(Arrays.stream(matrix).flatMapToInt(Arrays::stream).mapToObj(BigInteger::valueOf).reduce(ZERO,BigInteger::add));
-    }
+
 }
